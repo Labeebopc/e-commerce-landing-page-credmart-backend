22426f926f32aaa68791611bae029291a787cd42
Labeebopc <Labeebopc313@gmail.com>
2023-03-21 20:50:42 +0530

    implemented the logic to get data from backend

---
+++ data/data.js
@@ -0,0 +1,53 @@
+const data ={
+
+    Bag: [
+        {
+            "id": 1,
+            "name": "Reebok Dark Men's Bag",
+            "productImage": "images/reebokBag.png",
+            "description": "A flexible container with an opening at the top, used for carrying things.",
+            "price": "$199"
+        },
+        {
+            "id": 2,
+            "name": "Dolce & Gabbana",
+            "productImage": "images/dcBag.png",
+            "description": "A flexible container with an opening at the top, used for carrying things.",
+            "price": "$699"
+        },
+        {
+            "id": 3,
+            "name": "Louis Vuitton",
+            "productImage": "images/lvBag.png",
+            "description": "A flexible container with an opening at the top, used for carrying things.",
+            "price": "$999"
+        }
+    ],
+
+    Shoes: [
+        {
+            "id": 4,
+            "name": "Nike",
+            "productImage": "images/shoe.png",
+            "description": "A covering for the foot, typically made of leather, having a sturdy sole and not reaching above the ankle",
+            "price": "$99"
+        },
+        {
+            "id": 5,
+            "name": "Adidas",
+            "productImage": "images/adidasShoe.png",
+            "description": "A covering for the foot, typically made of leather.",
+            "price": "$199"
+        },
+
+        {
+            "id": 6,
+            "name": "Puma",
+            "productImage": "images/pumaShoe.png",
+            "description": "A covering for the foot, typically made of leather, having a sturdy sole and not reaching above the ankle",
+            "price": "$59"
+        }
+    ]
+}
+
+module.exports = data;
